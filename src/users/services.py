92a29d52fb2eb13031f8902b99c17be7a71c3e6a
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-21 00:25:22 +0200

    create session and auth services

--- src/users/services.py
+++ src/users/services.py
@@ -1,9 +1,24 @@
+import secrets
+from datetime import datetime, timedelta
+
 from src.users.exceptions import (
     UserNotFoundError,
     UserAlreadyExistsError,
+    InvalidCredentialsError,
 )
-from src.users.interfaces import UserServiceInterface, UserRepositoryInterface
-from src.users.schemas import UserResponseSerializer, UserCreateSerializer
+from src.users.interfaces import (
+    UserServiceInterface,
+    UserRepositoryInterface,
+    SessionServiceInterface,
+    SessionRepositoryInterface,
+    AuthServiceInterface,
+)
+from src.users.schemas import (
+    UserResponseSerializer,
+    UserCreateSerializer,
+    SessionCreateSerializer,
+    UserLoginSerializer,
+)
 
 
 class UserService(UserServiceInterface):
@@ -17,6 +32,7 @@ class UserService(UserServiceInterface):
         user = await self._user_repository.get_user_by_id(id=id)
         if user:
             return user
+
         raise UserNotFoundError(f"User with id: {id} not found")
 
     async def create_user(
@@ -33,3 +49,44 @@ class UserService(UserServiceInterface):
 
         new_user = await self._user_repository.register_user(user)
         return new_user
+
+
+class SessionService(SessionServiceInterface):
+    def __init__(self, session_repository: SessionRepositoryInterface):
+        self._session_repository = session_repository
+
+    @staticmethod
+    def _generate_session_token() -> str:
+        return secrets.token_urlsafe(32)
+
+    async def make_session(self, user_id: int) -> str:
+        session_create_serializer = SessionCreateSerializer(
+            token=self._generate_session_token(),
+            expired_at=datetime.now() + timedelta(days=1),
+            user_id=user_id
+        )
+        return await self._session_repository.create_session(
+            session_create_serializer
+        )
+
+
+class AuthService(AuthServiceInterface):
+    def __init__(
+        self, session_service: SessionServiceInterface,
+        user_repository: UserRepositoryInterface
+    ) -> None:
+        self._session_service = session_service
+        self._user_repository = user_repository
+
+    async def login(self, login_data: UserLoginSerializer) -> str:
+        user = await self._user_repository.authenticate_user(
+            email=login_data.email,
+            password=login_data.password
+        )
+
+        if not user:
+            raise InvalidCredentialsError
+
+        token = await self._session_service.make_session(user_id=user.id)
+
+        return token
