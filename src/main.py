61555b0159ce547230bb54f58e3f8338282c8888
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-21 00:21:45 +0200

    add static files resolver to main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
+from src.settings import STATIC_DIR
 from src.users.router import router as users_router
 
 
@@ -12,3 +14,6 @@ app.include_router(users_router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
