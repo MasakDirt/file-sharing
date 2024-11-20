d85f67f6f03797f24f75261fa7b5b2ef3d036040
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-21 00:21:18 +0200

    add new model - session for user authentication

--- src/users/models.py
+++ src/users/models.py
@@ -1,6 +1,9 @@
+import datetime
+
 import bcrypt
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.orm import validates
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import validates, relationship
 
 from src.database.base import Base
 from src.users.validators import validate_username, validate_email
@@ -12,10 +15,16 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String(300))
     email = Column(String(255), unique=True)
-    username = Column(String(255), unique=True)
+    username = Column(String(255), unique=True, index=True)
     _hashed_password = Column("hashed_password", String(255))
     is_staff = Column(Boolean, default=False)
     is_superuser = Column(Boolean, default=False)
+
+    sessions = relationship(
+        "Session",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
 
     def __str__(self) -> str:
         return f"User {self.username} with email: {self.email}"
@@ -49,3 +58,21 @@ class User(Base):
         print(key)
         validate_email(email)
         return email
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String(128), unique=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    expired_at = Column(DateTime, nullable=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+
+    user = relationship("User", back_populates="sessions")
+
+    def __str__(self):
+        return f"Token: {self.token} expire at: {self.expired_at}"
+
+    def is_expired(self):
+        return datetime.datetime.now() > self.expired_at
