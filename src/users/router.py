0f16a5f982e2ca28c28d075b2b001e1c7fc2d66b
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-21 00:27:16 +0200

    add login urls to router

--- src/users/router.py
+++ src/users/router.py
@@ -5,12 +5,17 @@ from src.users.controllers import (
     get_user_controller,
     get_sign_up_page,
     create_user_controller,
+    get_login_page,
+    login,
 )
 
 router = APIRouter()
 
-router.get("/users/")(get_all_users_controller)
-router.get("/users/{id}/")(get_user_controller)
+router.get("/users/", name="user-list")(get_all_users_controller)
+router.get("/users/{id}/", name="user-detail")(get_user_controller)
 
-router.get("/signUp/")(get_sign_up_page)
-router.post("/signUp/")(create_user_controller)
+router.get("/signUp/", name="get-sign-up")(get_sign_up_page)
+router.post("/signUp/", name="post-sign-up")(create_user_controller)
+
+router.get("/login/", name="get-login")(get_login_page)
+router.post("/login/", name="post-login")(login)
