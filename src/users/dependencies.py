60150cd52efde128f151d96ec8a655d6bdfa6f4e
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-21 00:26:38 +0200

    create such dependencies - 'get_session_repository', 'get_session_service' and 'get_auth_service'

--- src/users/dependencies.py
+++ src/users/dependencies.py
@@ -2,17 +2,39 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.session import get_db
-from src.users.repositories import UserRepository
-from src.users.services import UserService
+from src.users.repositories import UserRepository, SessionRepository
+from src.users.services import UserService, AuthService, SessionService
 
 
-async def get_user_repository(
+def get_user_repository(
     db: AsyncSession = Depends(get_db)
 ) -> UserRepository:
     return UserRepository(db=db)
 
 
-async def get_user_service(
+def get_user_service(
     user_repository: UserRepository = Depends(get_user_repository)
 ) -> UserService:
     return UserService(user_repository=user_repository)
+
+
+def get_session_repository(
+    db: AsyncSession = Depends(get_db)
+) -> SessionRepository:
+    return SessionRepository(db=db)
+
+
+def get_session_service(
+    session_repository: SessionRepository = Depends(get_session_repository)
+) -> SessionService:
+    return SessionService(session_repository=session_repository)
+
+
+def get_auth_service(
+    user_repository: UserRepository = Depends(get_user_repository),
+    session_service: SessionService = Depends(get_session_service)
+) -> AuthService:
+    return AuthService(
+        user_repository=user_repository,
+        session_service=session_service
+    )
