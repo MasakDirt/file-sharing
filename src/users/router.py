2106ad62c99523159a9cfa6afe8595075775500e
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-22 00:48:39 +0200

    add logout endpoint to router.py

--- src/users/router.py
+++ src/users/router.py
@@ -7,6 +7,7 @@ from src.users.controllers import (
     create_user_controller,
     get_login_page,
     login,
+    logout,
 )
 
 router = APIRouter()
@@ -14,8 +15,10 @@ router = APIRouter()
 router.get("/users/", name="user-list")(get_all_users_controller)
 router.get("/users/{id}/", name="user-detail")(get_user_controller)
 
-router.get("/signUp/", name="get-sign-up")(get_sign_up_page)
-router.post("/signUp/", name="post-sign-up")(create_user_controller)
+router.get("/register/", name="get-sign-up")(get_sign_up_page)
+router.post("/register/", name="post-sign-up")(create_user_controller)
 
 router.get("/login/", name="get-login")(get_login_page)
 router.post("/login/", name="post-login")(login)
+
+router.get("/logout/", name="logout")(logout)
