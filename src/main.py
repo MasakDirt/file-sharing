d6a80faeb07ecc308d1d40352da96ba25def75df
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-22 00:46:10 +0200

    add new middlewares to main.py and refactor root endpoint

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
-from src.settings import STATIC_DIR
+from src.middlewares import authentication_middleware, add_user_to_templates
+from src.settings import STATIC_DIR, TEMPLATES
 from src.users.router import router as users_router
 
 
 app = FastAPI()
 
-
 app.include_router(users_router)
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+app.middleware("http")(authentication_middleware)
+app.middleware("http")(add_user_to_templates)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+async def root(request: Request) -> TEMPLATES.TemplateResponse:
+    return TEMPLATES.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
