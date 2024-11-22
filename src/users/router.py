9b79fe4b843a5fd0d5e71521e387953e276ce616
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-22 18:25:06 +0200

    refactor router for getting 'me'

--- src/users/router.py
+++ src/users/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from src.users.controllers import (
     get_all_users_controller,
-    get_user_controller,
+    get_me_controller,
     get_sign_up_page,
     create_user_controller,
     get_login_page,
@@ -13,7 +13,7 @@ from src.users.controllers import (
 router = APIRouter()
 
 router.get("/users/", name="user-list")(get_all_users_controller)
-router.get("/users/{id}/", name="user-detail")(get_user_controller)
+router.get("/users/me/", name="user-detail")(get_me_controller)
 
 router.get("/register/", name="get-sign-up")(get_sign_up_page)
 router.post("/register/", name="post-sign-up")(create_user_controller)
