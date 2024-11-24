a03b28315dc3e5ea3228ef974cb879bf0d04dfa2
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-24 21:06:44 +0200

    implement allowed files for user service

--- src/files/services.py
+++ src/files/services.py
@@ -4,11 +4,19 @@ import aiofiles
 from fastapi import UploadFile
 
 from src.database.utils import serialize_obj
-from src.files.interfaces import FileServiceInterface, FileRepositoryInterface
+from src.files.interfaces import (
+    FileServiceInterface,
+    FileRepositoryInterface,
+    AllowedFilesForUserServiceInterface,
+    AllowedFilesForUserRepositoryInterface,
+)
+from src.files.models import AllowedFilesForUser
 from src.files.schemas import FileCreateSerializer, FileResponseSerializer
 from src.files.utils import make_unique_filename
 from src.files.validators import validate_file_size
 from src.settings import UPLOADED_DIR
+from src.users.interfaces import UserRepositoryInterface
+from src.users.models import User
 
 
 class FileService(FileServiceInterface):
@@ -39,3 +47,60 @@ class FileService(FileServiceInterface):
             FileResponseSerializer(**serialize_obj(file))
             for file in files
         ]
+
+    async def remove_file(self, id: int) -> None:
+        file = await self._file_repository.get_file(id=id)
+        await self._file_repository.delete_file(id=id)
+        if file:
+            file_path = os.path.join(UPLOADED_DIR, file.new_file_name)
+            os.remove(file_path)
+
+
+class AllowedFilesForUserService(AllowedFilesForUserServiceInterface):
+    def __init__(
+        self, allowed_repository: AllowedFilesForUserRepositoryInterface,
+        user_repository: UserRepositoryInterface
+    ) -> None:
+        self._allowed_repository = allowed_repository
+        self._user_repository = user_repository
+
+    async def add_file_access(
+        self, file_id: int,
+        user_ids: set[int]
+    ) -> None:
+        allowed_files = [
+            AllowedFilesForUser(user_id=user_id, allowed_file_id=file_id)
+            for user_id in user_ids
+        ]
+        await self._allowed_repository.create_files_access(allowed_files)
+
+    async def remove_file_access(
+        self, file_id: int,
+        user_ids: set[int]
+    ) -> None:
+        await self._allowed_repository.delete_files_access(file_id, user_ids)
+
+    async def get_users_with_access_to_file(self, file_id: int) -> list[User]:
+        users = await self._user_repository.get_all_users(with_admins=False)
+        allowed_user_ids = await self._allowed_repository.get_users_with_access_to_file(
+            file_id
+        )
+
+        for user in users:
+            user.has_access = user.id in allowed_user_ids
+
+        return users
+
+    async def update_file_access(
+        self, file_id: int,
+        selected_user_ids: set[int]
+    ) -> None:
+        current_user_ids = set(
+            await self._allowed_repository.get_users_with_access_to_file(file_id)
+        )
+
+        users_to_add = selected_user_ids - current_user_ids
+        users_to_remove = current_user_ids - selected_user_ids
+
+        await self.add_file_access(file_id, users_to_add)
+        await self.remove_file_access(file_id, users_to_remove)
