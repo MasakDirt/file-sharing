1529bbd716c2dfd82a23fa82b4ec5be2737e5b21
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-24 21:05:41 +0200

    add for allowed files for user repository and service

--- src/files/interfaces.py
+++ src/files/interfaces.py
@@ -1,9 +1,11 @@
 from abc import abstractmethod, ABC
+from typing import Optional, Sequence
 
 from fastapi import UploadFile
 
-from src.files.models import File
+from src.files.models import File, AllowedFilesForUser
 from src.files.schemas import FileCreateSerializer, FileResponseSerializer
+from src.users.models import User
 
 
 class FileRepositoryInterface(ABC):
@@ -27,4 +29,56 @@ class FileServiceInterface(ABC):
 
     @abstractmethod
     async def get_all_files(self) -> list[FileResponseSerializer]:
+        pass
+
+    @abstractmethod
+    async def remove_file(self, id: int) -> None:
+        pass
+
+
+class AllowedFilesForUserRepositoryInterface(ABC):
+    @abstractmethod
+    async def create_files_access(
+        self, allowed_files: list[AllowedFilesForUser]
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def delete_files_access(
+        self, file_id: int,
+        user_ids: set[int],
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def get_users_with_access_to_file(
+        self, file_id: int
+    ) -> Sequence[int]:
+        pass
+
+
+class AllowedFilesForUserServiceInterface(ABC):
+    @abstractmethod
+    async def add_file_access(
+        self, file_id: int,
+        user_ids: set[int]
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def remove_file_access(
+        self, file_id: int,
+        user_ids: set[int]
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def get_users_with_access_to_file(self, file_id: int) -> list[User]:
+        pass
+
+    @abstractmethod
+    async def update_file_access(
+        self, file_id: int,
+        selected_user_ids: set[int]
+    ) -> None:
         pass
