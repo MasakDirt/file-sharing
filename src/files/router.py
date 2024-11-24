cf506f9e2243bfd3a8eb3d0925323f98771d91e9
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-24 21:08:20 +0200

    add to router remove file, get giving access page and update access endpoints

--- src/files/router.py
+++ src/files/router.py
@@ -4,13 +4,23 @@ from src.files.controllers import (
     create_file_controller,
     get_file_upload_page_controller,
     get_all_files_controller,
+    remove_file,
+    get_file_giving_access_page,
+    update_users_file_access,
 )
 
 router = APIRouter()
 
+
+router.get("/files/", name="file-list")(get_all_files_controller)
 router.get("/files/upload/", name="get-upload-file")(
     get_file_upload_page_controller
 )
 router.post("/files/upload/", name="upload-file")(create_file_controller)
-
-router.get("/files/", name="file-list")(get_all_files_controller)
+router.post("/files/{id}/delete/", name="file-remove")(remove_file)
+router.get("/files/{file_id}/access/", name="get-file-access")(
+    get_file_giving_access_page
+)
+router.post("/files/{file_id}/access/", name="file-access")(
+    update_users_file_access
+)
