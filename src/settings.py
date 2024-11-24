5fb185cd6ee1f958d56c0511b1b368208932a553
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-24 16:49:53 +0200

    add settings for files

--- src/settings.py
+++ src/settings.py
@@ -16,10 +16,21 @@ HOST = os.getenv("HOST")
 
 DATABASE_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{HOST}:3306/{MYSQL_DATABASE}"
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "True"
 
-# Get important dirs paths
+# Get important dir paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_DIR = os.path.join(BASE_DIR, "static")
+
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = Jinja2Templates(directory=TEMPLATES_DIR)
+
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
+UPLOADED_DIR = os.path.join(MEDIA_DIR, "uploaded")
+
+
+# File limitations
+ALLOWED_EXTENSIONS = {".txt", ".pdf", ".docx", ".pptx",
+                      ".yaml", ".py", ".xml", ".json"}
+MAX_FILE_SIZE = 2 * 1024 * 1024
