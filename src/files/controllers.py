a778252fe668c8c504cc79cd5e62b18a4755b6d3
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-24 21:07:51 +0200

    add to controllers remove file, get giving access page and update access

--- src/files/controllers.py
+++ src/files/controllers.py
@@ -2,8 +2,14 @@ from fastapi import Request, HTTPException, Depends, UploadFile, File
 from starlette import status
 from starlette.responses import RedirectResponse
 
-from src.files.dependencies import get_file_service
-from src.files.interfaces import FileServiceInterface
+from src.files.dependencies import (
+    get_file_service,
+    get_allowed_files_for_user_service,
+)
+from src.files.interfaces import (
+    FileServiceInterface,
+    AllowedFilesForUserServiceInterface,
+)
 from src.settings import TEMPLATES, ALLOWED_EXTENSIONS
 from src.utils.decorators import admin_only
 
@@ -35,7 +41,7 @@ async def create_file_controller(
         )
 
     return RedirectResponse(
-        url="/files/upload/",
+        url="/files/",
         status_code=status.HTTP_302_FOUND
     )
 
@@ -52,3 +58,49 @@ async def get_all_files_controller(
         name="files/files_list.html",
         context={"files": all_files}
     )
+
+
+@admin_only
+async def remove_file(
+    request: Request,
+    id: int,
+    file_service: FileServiceInterface = Depends(get_file_service)
+) -> RedirectResponse:
+    await file_service.remove_file(id=id)
+
+    return RedirectResponse(
+        url="/files/",
+        status_code=status.HTTP_302_FOUND
+    )
+
+
+@admin_only
+async def get_file_giving_access_page(
+    request: Request,
+    file_id: int,
+    allowed_files_service: AllowedFilesForUserServiceInterface = Depends(
+        get_allowed_files_for_user_service
+    )
+) -> TEMPLATES.TemplateResponse:
+    users = await allowed_files_service.get_users_with_access_to_file(file_id)
+    return TEMPLATES.TemplateResponse(
+        request=request,
+        name="files/give_file_access.html",
+        context={"users": users, "file_id": file_id}
+    )
+
+
+@admin_only
+async def update_users_file_access(
+    request: Request,
+    file_id: int,
+    file_access_service: AllowedFilesForUserServiceInterface = Depends(
+        get_allowed_files_for_user_service
+    )
+) -> RedirectResponse:
+    form = await request.form()
+    selected_user_ids = set(map(int, form.getlist("user_ids")))
+
+    await file_access_service.update_file_access(file_id, selected_user_ids)
+
+    return RedirectResponse(url="/files/", status_code=status.HTTP_302_FOUND)
